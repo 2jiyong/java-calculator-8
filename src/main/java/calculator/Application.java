82bf0c82760f098eb211a1c85b99c57bfc6a81ac
2jiyong <jdragon2000@naver.com>
2025-10-16 21:08:31 +0900

    feat: implement basic input reading logic
    Add Scanner for reading user input from console.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
     }
 }
