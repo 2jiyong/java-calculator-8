bfefb6782a4f467dfe9eca7c7f44656fbb9cbe0c
2jiyong <jdragon2000@naver.com>
2025-10-19 00:26:10 +0900

    fix: replace Scanner with Console for user input handling

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,13 +2,14 @@ package calculator;
 
 import java.util.Scanner;
 import java.util.regex.Pattern;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.\n");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
+        // Scanner sc = new Scanner(System.in);
+        String input = Console.readLine();
 
         int[] numbersArray = splitByDelimiter(input);
         int result = sum(numbersArray);
